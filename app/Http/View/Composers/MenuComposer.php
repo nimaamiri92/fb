52ed3434a709f52cf9410157c085eb6b329a4d93
Nima Amiri <nimaamiri92@gmail.com>
2021-07-11 19:17:32 +0430

    fix

--- app/Http/View/Composers/MenuComposer.php
+++ app/Http/View/Composers/MenuComposer.php
@@ -3,16 +3,26 @@
 namespace App\Http\View\Composers;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class MenuComposer
 {
     public function compose(View $view)
     {
-        $view->with('menus', Category::with('children')
-            ->whereIn('type', [Category::MENU])
-            ->where('status', Category::ACTIVE)
-            ->orderBy('order', 'asc')
-            ->get());
+        if (Cache::has('menu')) {
+            return Cache::get('menu');
+        }
+
+
+        Cache::remember('menu', 3600, function () use ($view) {
+            $view->with('menus', Category::with('children')
+                ->whereIn('type', [Category::MENU])
+                ->where('status', Category::ACTIVE)
+                ->orderBy('order', 'asc')
+                ->get());
+        });
+
+        return Cache::get('menu');
     }
 }
