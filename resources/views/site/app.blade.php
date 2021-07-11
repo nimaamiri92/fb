de56dbbb9055847d663a718081a7d7b0360856ae
Nima Amiri <nimaamiri92@gmail.com>
2021-07-11 19:21:21 +0430

    fix

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     @include('site.layouts.style')
 </head>
+<meta name="description" content="خرید اینترنتی لباس نوزاد را با فروشگاه آنلاین بی‌سی‌سی تجربه کنید. انواع لباس نوزاد دختر و پسر در طرح‌های شاد و رنگارنگ با کیفیت جهانی">
 <body dir="rtl">
 @include('site.layouts.header')
 @include('site.layouts.nav')
